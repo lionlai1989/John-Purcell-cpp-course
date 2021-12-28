4c20724ea8c228cf069bb4f11952b1623994be60
lai <lionlai1989@gmail.com>
2021-12-28 10:28:42 +0100

    update

---
+++ cpp11_features/casting/static_cast.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Parent {
+};
+
+class Brother : public Parent {
+};
+
+class Sister : public Parent {
+};
+
+// Example command: g++ -Wall -std=c++11 -std=gnu++11 static_cast.cpp && ./a.out
+int main() {
+
+    return 0;
+}
