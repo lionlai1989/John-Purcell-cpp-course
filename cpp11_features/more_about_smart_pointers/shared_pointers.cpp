54bedd923994ffad069c73429ba5f518bbfbc5eb
lai <lionlai1989@gmail.com>
2022-01-06 06:38:58 +0100

    update

--- cpp11_features/more_about_smart_pointers/shared_pointers.cpp
+++ cpp11_features/more_about_smart_pointers/shared_pointers.cpp
@@ -20,11 +20,58 @@ class Test {
 
 // Example command: g++ -Wall -std=c++11 -std=gnu++11 shared_pointers.cpp && ./a.out
 int main() {
-    shared_ptr<Test> p_test1(new Test());
+    /**
+     * shared_ptr:
+     * std::shared_ptr is a smart pointer that retains shared ownership of an object through a
+     * pointer. Several shared_ptr objects may own the same object. The object is destroyed and its
+     * memory deallocated when either of the following happens: 
+     * - the last remaining shared_ptr owning the object is destroyed;
+     * - the last remaining shared_ptr owning the object is assigned another pointer via operator=
+     *   or reset().
+     * 
+     * Following on from the tutorial on unique pointers, shared pointers are really similar. shared
+     * pointers just add a little twist which is that they don't delete the memory associated with
+     * your objects until all the pointers that point at that object have gone out of scope. 
+     * 
+     * At the moment, shared_ptr cannot point to arrays, but that might change in C++17, but here in
+     * 2015 we can only use shared pointers to point to an individual object.
+     */
+    {
+        shared_ptr<Test> p_test1(new Test());
+        p_test1->greet();
+        /**
+         * p_test1 is out of scope when it reaches the end of the closing curly brackets.
+         * 
+         * Output:
+         * Created
+         * Hello
+         * Destroyed
+         */
+    }
 
-    shared_ptr<Test> p_test2 = make_shared<Test>();
-
-    shared_ptr<Test> p_test3 = p_test2;
+    /**
+     * Now, according to the documentation, the kind of the best way to create a shared pointer is
+     * to use make_shared template function. It is more efficient than using "p_test1(new Test())".
+     * 
+     * make_shared:
+     * Allocates and constructs an object of type T passing args to its constructor, and returns an
+     * object of type shared_ptr<T> that owns and stores a pointer to it (with a use count of 1). 
+     */
+    shared_ptr<Test> p_test3 = nullptr;
+    {
+        shared_ptr<Test> p_test2 = make_shared<Test>();
+        p_test3 = p_test2;
+    }
+    /**
+     * What is happening here is that when p_test2 is out of scope, the Test object still exists
+     * because p_test3 which also points to the same Test object have not gone out of scope. That is
+     * why the Test object is being destroyed at the end of the program.
+     *
+     * Output:
+     * Created
+     * Finished
+     * Destroyed
+     */
 
     cout << "Finished" << endl;
 
--- cpp11_features/more_about_smart_pointers/unique_pointers.cpp
+++ cpp11_features/more_about_smart_pointers/unique_pointers.cpp
@@ -20,39 +20,106 @@ class Test {
 
 class Temp {
   private:
+    /**
+     * Here, we are not allowed to allocate memory here as below.
+     * unique_ptr<Test[]> p_test(new Test[2]);
+     * We have to allocate memory in the public constructor.
+     */
     unique_ptr<Test[]> p_test;
 
   public:
     Temp() : p_test(new Test[2]) {
     }
+
+    /**
+     * NOTE: There is no destructor here and the Test object will be destroyed anyway when the
+     * object is out of scope.
+     */
 };
 
 // Example command: g++ -Wall -std=c++11 -std=gnu++11 unique_pointers.cpp && ./a.out
 int main() {
+    /**
+     * unique_ptr:
+     * std::unique_ptr is a smart pointer that owns and manages another object through a pointer and
+     * disposes of that object when the unique_ptr goes out of scope. The object is disposed of,
+     * using the associated deleter when either of the following happens:
+     * - the managing unique_ptr object is destroyed
+     * - the managing unique_ptr object is assigned another pointer via operator= or reset().
+     * 
+     * unique_ptr is a template type. The bracket needs to hold what type of things is going to
+     * point to. And the asterisk "*"" is not needed here, because it knows it is a pointer already.
+     * 
+     * Now we want p_test1 to manage the memory that it allocates with new itself. We cannot just
+     * say p_test1 equals new int, "p_test1 = new int". If we want to do that we have to do the
+     * memory allocation within the constructor brackets, "p_test1(new int)" 
+     */
     unique_ptr<int> p_test1(new int);
     *p_test1 = 7;
-    cout << *p_test1 << endl;
-
-    unique_ptr<Test> p_test2(new Test);
-    p_test2->greet();
+    cout << *p_test1 << endl; // 7
 
     {
-        unique_ptr<Test> p_test3(new Test);
-        p_test3->greet();
+        unique_ptr<Test> p_test2(new Test);
+        p_test2->greet();
+        /**
+         * What happens here is that this local variable, p_test2, goes out of scope when it hits
+         * the closing bracket "}" (Line 57), and at that point the destructor is called and the
+         * memory that we allocated is automatically cleaned up by the unique_ptr.
+         * 
+         * Output:
+         * Created
+         * Hello
+         * Destroyed
+         */
     }
+
+    unique_ptr<Test> p_test3(new Test);
+    p_test3->greet();
+    /** 
+     * The destructor will not be called at this point.
+     * Output:
+     * Created
+     * Hello
+     */
 
     /**
      * Do not use auto_ptr which is deprecated.
      * auto_ptr<Test> p_test4(new Test);
-     * p_test4->greet();
      */
 
-    unique_ptr<Test[]> p_test5(new Test[3]);
+    /**
+     * Now an array, Test[], is put in the unique_ptr's template brackets and 2 Test objects are
+     * allocated.
+     */
+    unique_ptr<Test[]> p_test5(new Test[2]);
     p_test5[1].greet();
+    /**
+     * Two Test objects are created.
+     * Output:
+     * Created
+     * Created
+     * Hello
+     */
 
     Temp temp;
+    /**
+     * Two Test objects are created in the temp object. No destructor is called at this point.
+     * Output:
+     * Created
+     * Created
+     */
 
-    cout << "Finished" << endl;
+    cout << "main() Finished" << endl;
+    /**
+     * There are five objects being destroyed when the program is reaching the end (out-of-scope).
+     * Output:
+     * main() Finished
+     * Destroyed
+     * Destroyed
+     * Destroyed
+     * Destroyed
+     * Destroyed
+     */
 
     return 0;
 }
