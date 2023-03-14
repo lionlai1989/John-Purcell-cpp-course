ae4f7f4348efc92a8322f91e3d9f729cf749a715
lai <lionlai1989@gmail.com>
2023-03-14 04:02:21 +0100

    update

--- cpp11_features/more_about_constructors/constructors_and_assignment_operators.cpp
+++ cpp11_features/more_about_constructors/constructors_and_assignment_operators.cpp
@@ -3,8 +3,6 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 class String1 {
 private:
   uint32_t size;
@@ -12,7 +10,7 @@ private:
 
 public:
   String1() {
-    cout << "Default No-Parameter Constructor" << endl;
+    std::cout << "Default No-Parameter Constructor\n";
     this->size = 0;
     this->buffer = nullptr;
     /**
@@ -25,7 +23,7 @@ public:
   }
 
   String1(const char *name) {
-    cout << "Parameterized Constructor" << endl;
+    std::cout << "Parameterized Constructor\n";
     this->size = std::strlen(name);
     // No need to allocate size+1 buffer. Why?
     this->buffer = new char[this->size];
@@ -33,14 +31,14 @@ public:
   }
 
   String1(const String1 &other) {
-    cout << "Copy Construtor" << endl;
+    std::cout << "Copy Construtor\n";
     this->size = other.size;
     this->buffer = new char[this->size];
     memcpy(this->buffer, other.buffer, this->size);
   }
 
   String1 &operator=(const String1 &other) {
-    cout << "Copy Assignment Operator" << endl;
+    std::cout << "Copy Assignment Operator\n";
     if (this != &other) {
       // The buffer size of `this` is different from the `other`'s. So we need
       // to delete `this` and reallocate memory for `this` with the size of
@@ -55,18 +53,18 @@ public:
   }
 
   ~String1() {
-    cout << "Destructor: ";
+    std::cout << "Destructor: ";
     for (uint32_t i = 0; i < this->size; ++i) {
-      cout << this->buffer[i];
+      std::cout << this->buffer[i];
     }
-    cout << '\n';
+    std::cout << '\n';
     delete[] this->buffer;
   }
 
-  friend ostream &operator<<(ostream &out, const String1 &test);
+  friend std::ostream &operator<<(std::ostream &out, const String1 &test);
 };
 
-ostream &operator<<(ostream &out, const String1 &test) {
+std::ostream &operator<<(std::ostream &out, const String1 &test) {
   out << "String: ";
   for (uint32_t i = 0; i < test.size; ++i) {
     out << test.buffer[i] << ' ';
@@ -83,27 +81,27 @@ private:
 
 public:
   String2() {
-    cout << "Default No-Parameter Constructor" << endl;
+    std::cout << "Default No-Parameter Constructor\n";
     this->size = 0;
     this->buffer = nullptr;
   }
 
   String2(const char *name) {
-    cout << "Parameterized Constructor" << endl;
+    std::cout << "Parameterized Constructor\n";
     this->size = std::strlen(name);
     this->buffer = new char[this->size];
     memcpy(this->buffer, name, this->size);
   }
 
   String2(const String2 &other) {
-    cout << "Copy Construtor" << endl;
+    std::cout << "Copy Construtor\n";
     this->size = other.size;
     this->buffer = new char[this->size];
     memcpy(this->buffer, other.buffer, this->size);
   }
 
   String2 &operator=(const String2 &other) {
-    cout << "Copy Assignment Operator" << endl;
+    std::cout << "Copy Assignment Operator\n";
     if (this != &other) {
       // The buffer size of `this` is different from the `other`'s. So we need
       // to delete `this` and reallocate memory for `this` with the size of
@@ -118,7 +116,7 @@ public:
   }
 
   String2(String2 &&other) {
-    cout << "Move Constructor" << endl;
+    std::cout << "Move Constructor\n";
     this->size = other.size;
     /**
      * In the copy constructor, we have to allocate memory.
@@ -138,7 +136,7 @@ public:
   }
 
   String2 &operator=(String2 &&other) {
-    cout << "Move Assignment Operator" << endl;
+    std::cout << "Move Assignment Operator\n";
     if (this != &other) {
       this->size = other.size;
       /**
@@ -156,21 +154,21 @@ public:
     return *this;
   }
   ~String2() {
-    cout << "Destructor: ";
+    std::cout << "Destructor: ";
     // NOTE: a potential bug is here. If `this` has pointed to a nullptr and
     // size is non-zero, then it causes error:
     // Segmentation fault (core dumped)
     for (uint32_t i = 0; i < this->size; ++i) {
-      cout << this->buffer[i];
+      std::cout << this->buffer[i];
     }
-    cout << '\n';
+    std::cout << '\n';
     delete[] this->buffer;
   }
 
-  friend ostream &operator<<(ostream &out, const String2 &test);
+  friend std::ostream &operator<<(std::ostream &out, const String2 &test);
 };
 
-ostream &operator<<(ostream &out, const String2 &test) {
+std::ostream &operator<<(std::ostream &out, const String2 &test) {
   out << "String: ";
   for (uint32_t i = 0; i < test.size; ++i) {
     out << test.buffer[i] << ' ';
@@ -180,101 +178,184 @@ ostream &operator<<(ostream &out, const String2 &test) {
 
 String2 get_String2() { return String2(); }
 
+class String3 {
+private:
+  /**
+   * Does the order of declaration matter?
+   */
+  std::size_t size = 0;
+  char *buffer = nullptr;
+
+public:
+  String3() : size{0}, buffer{nullptr} {
+    std::clog << "Default No-Parameter Constructor\n";
+  }
+
+  String3(const char *name) : size{std::strlen(name)}, buffer{new char[size]} {
+    /**
+     * Pay attention to the order of the initialization.
+     */
+    std::clog << "Parameterized Constructor\n";
+    memcpy(this->buffer, name, this->size);
+  }
+
+  String3(const String3 &other) : size{other.size}, buffer{new char[size]} {
+    /**
+     * Pay attention to the order of the initialization.
+     */
+    std::clog << "Copy Construtor\n";
+    memcpy(this->buffer, other.buffer, this->size);
+  }
+
+  String3 &operator=(const String3 &other) {
+    std::clog << "Copy Assignment Operator\n";
+    if (this != &other) {
+      delete[] this->buffer;
+
+      this->size = other.size;
+      this->buffer = new char[this->size];
+      memcpy(this->buffer, other.buffer, this->size);
+    }
+    return *this;
+  }
+
+  String3(String3 &&other) {
+    std::cout << "Move Constructor\n";
+    swap(other);
+  }
+
+  String3 &operator=(String3 &&other) {
+    std::cout << "Move Assignment Operator\n";
+    swap(other);
+    other.size = 0;
+    other.buffer = nullptr;
+    return *this;
+  }
+
+  void swap(String3 &other) {
+    std::swap(this->size, other.size);
+    std::swap(this->buffer, other.buffer);
+  }
+
+  ~String3() {
+    std::cout << "Destructor: ";
+    // NOTE: a potential bug is here. If `this` has pointed to a nullptr and
+    // size is non-zero, then it causes error:
+    // Segmentation fault (core dumped)
+    for (uint32_t i = 0; i < this->size; ++i) {
+      std::cout << this->buffer[i];
+    }
+    std::cout << '\n';
+    delete[] this->buffer;
+  }
+
+  friend std::ostream &operator<<(std::ostream &out, const String3 &test);
+};
+
+std::ostream &operator<<(std::ostream &out, const String3 &test) {
+  out << "String: ";
+  for (uint32_t i = 0; i < test.size; ++i) {
+    out << test.buffer[i] << ' ';
+  }
+  return out;
+}
+
+String3 get_String3() { return String3(); }
+
 // Example command:
 // g++ -Wall -std=c++17 -std=gnu++17 constructors_and_assignment_operators.cpp && ./a.out
 int main() {
-  // Comment out the code below to run the test.
+  /**
+   * Comment out the code below to run the test.
+   */
 
+  /**
+   * String1 does not have customized move constructor and move assignment
+   * operator. Observe the behaviour and explain what you see.
+   */
   // String1 test1;
   // test1 = get_String1();
   // String1 test2{"test2"};
   // String1 test3{test2};
-  // cout << "test1: " << test1 << endl;
+  // std::cout << "test1: " << test1 << '\n';
   // test1 = test3;
-  // cout << "test2: " << test2 << endl;
-  // cout << "test3: " << test3 << endl;
-  // cout << "test1: " << test1 << endl;
-  // vector<String1> vec;
+  // std::cout << "test2: " << test2 << '\n';
+  // std::cout << "test3: " << test3 << '\n';
+  // std::cout << "test1: " << test1 << '\n';
+  // std::vector<String1> vec;
   // vec.push_back(String1("rvalue"));
   // vec.push_back(test1);
-  // cout << "vec[0] " << vec[0] << endl;
-  // cout << "vec[1] " << vec[1] << endl;
-  /** The output message:
-  Default No-Parameter Constructor
-  Default No-Parameter Constructor
-  Copy Assignment Operator
-  Destructor: Destroy default
-  Parameterized Constructor
-  Copy Construtor
-  test1: Elements: 0 0 0 0 0 0 0 0 0 0
-  Copy Assignment Operator
-  test2: Elements: 1 1 1 1 1 1 1 1 1 1
-  test3: Elements: 1 1 1 1 1 1 1 1 1 1
-  test1: Elements: 1 1 1 1 1 1 1 1 1 1
-  Parameterized Constructor
-  Copy Construtor
-  Destructor: Destroy rvalue
-  Copy Construtor
-  Copy Construtor
-  Destructor: Destroy rvalue
-  vec[0] Elements: 100 100 100 100 100 100 100 100 100 100
-  vec[1] Elements: 1 1 1 1 1 1 1 1 1 1
-  Destructor: Destroy rvalue
-  Destructor: Destroy test2
-  Destructor: Destroy test2
-  Destructor: Destroy test2
-  Destructor: Destroy test2
-  **/
+  // std::cout << "vec[0] " << vec[0] << '\n';
+  // std::cout << "vec[1] " << vec[1] << '\n';
+  // std::cout << '\n';
 
+  // std::cout << "Test move-from object:\n";
+  // test2 = std::move(test1);
+  // std::cout << "test2: " << test2 << '\n';
+  // std::cout << "test1: " << test1 << '\n';
+
+  /**
+   * String2 has customized move constructor and move assignment
+   * operator. Observe the behaviour and explain what you see.
+   */
   // String2 test1;
   // test1 = get_String2();
   // String2 test2{"test2"};
   // String2 test3{test2};
-  // cout << "test1: " << test1 << endl;
+  // std::cout << "test1: " << test1 << '\n';
   // test1 = test3;
-  // cout << "test2: " << test2 << endl;
-  // cout << "test3: " << test3 << endl;
-  // cout << "test1: " << test1 << endl;
-  // vector<String2> vec;
+  // std::cout << "test2: " << test2 << '\n';
+  // std::cout << "test3: " << test3 << '\n';
+  // std::cout << "test1: " << test1 << '\n';
+  // std::vector<String2> vec;
   // vec.push_back(String2("rvalue"));
   // vec.push_back(test1);
-  // cout << "vec[0] " << vec[0] << endl;
-  // cout << "vec[1] " << vec[1] << endl;
-  /** The output message:
-  Default No-Parameter Constructor
-  Default No-Parameter Constructor
-  Move Assignment Operator
-  Destructor: Destroy default
-  Parameterized Constructor
-  Copy Construtor
-  test1: Elements: 0 0 0 0 0 0 0 0 0 0
-  Copy Assignment Operator
-  test2: Elements: 1 1 1 1 1 1 1 1 1 1
-  test3: Elements: 1 1 1 1 1 1 1 1 1 1
-  test1: Elements: 1 1 1 1 1 1 1 1 1 1
-  Parameterized Constructor
-  Move Constructor
-  Destructor: Destroy rvalue
-  Copy Construtor
-  Copy Construtor
-  Destructor: Destroy rvalue
-  vec[0] Elements: 100 100 100 100 100 100 100 100 100 100
-  vec[1] Elements: 1 1 1 1 1 1 1 1 1 1
-  Destructor: Destroy rvalue
-  Destructor: Destroy test2
-  Destructor: Destroy test2
-  Destructor: Destroy test2
-  Destructor: Destroy test2
-  **/
+  // std::cout << "vec[0] " << vec[0] << '\n';
+  // std::cout << "vec[1] " << vec[1] << '\n';
+  // std::cout << '\n';
 
-  // cout << "Test move-from object\n";
-  // String2 test1{"apple"};
-  // String2 test2;
-  // cout << test1 << '\n';
-  // cout << test2 << '\n';
+  // std::cout << "Test move-from object:\n";
   // test2 = std::move(test1);
-  // cout << test1 << '\n';
-  // cout << test2 << '\n';
+  // std::cout << "test2: " << test2 << '\n';
+  // std::cout << "test1: " << test1 << '\n';
+
+  /**
+   * String3 also has customized move constructor and move assignment
+   * operator and the implementation of String3 is optimized by following the
+   * suggestions in this codereview:
+   * https://codereview.stackexchange.com/questions/283826/a-simple-string-class-and-its-special-member-functions
+   * Compare the output message between String2 and String3 to see if there is
+   * any discrepancy.
+   */
+  String3 test1;
+  test1 = get_String3();
+  String3 test2{"test2"};
+  String3 test3{test2};
+  std::cout << "test1: " << test1 << '\n';
+  test1 = test3;
+  std::cout << "test2: " << test2 << '\n';
+  std::cout << "test3: " << test3 << '\n';
+  std::cout << "test1: " << test1 << '\n';
+  std::vector<String3> vec;
+  vec.push_back(String3("rvalue"));
+  vec.push_back(test1);
+  std::cout << "vec[0] " << vec[0] << '\n';
+  std::cout << "vec[1] " << vec[1] << '\n';
+  std::cout << '\n';
+
+  std::cout << "Test move-from object:\n";
+  test2 = std::move(test1);
+  std::cout << "test2: " << test2 << '\n';
+  std::cout << "test1: " << test1 << '\n';
+
+  std::cout << "\nTest move-from object\n";
+  String3 test_apple{"apple"};
+  String3 test_empty{"orange"};
+  std::cout << test_apple << '\n';
+  std::cout << test_empty << '\n';
+  test_empty = std::move(test_apple);
+  std::cout << test_apple << '\n';
+  std::cout << test_empty << '\n';
 
   return 0;
 }
