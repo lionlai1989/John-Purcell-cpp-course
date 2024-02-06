b15cefa06e3cd3a63ec327ab87c51c7f0554eabd
lai <lionlai1989@gmail.com>
2024-02-06 03:49:45 +0100

    update

--- exceptions/basic_exceptions.cpp
+++ exceptions/basic_exceptions.cpp
@@ -22,6 +22,19 @@ class Test {
         //
         throw MyException();
     }
+    void throw_unknown_exception() {
+        /**
+         * Notice that Divide by zero is undefined behaviour, and undefined behaviour is
+         * NOT exceptional behaviour (in any language). If my code has undefined
+         * behaviour then my entire program is invalid. There is NOTHING I can do to
+         * save my program. Even with `catch (...)` and `throw`, my program still crashes.
+         * 
+         * Fortunately, it outputs the following message:
+         * Floating point exception (core dumped)
+         */
+        // cout << (1 / 0) << endl; // Uncomment to see the undefined behaviour.
+        throw "Unknown exception occurred";
+    }
 };
 
 void might_go_wrong() {
@@ -66,21 +79,27 @@ void goes_wrong() {
 }
 
 int main() {
-    // Importance of catching order of exceptions. bad_alloc class is the subclasss of exception
-    // class. Thus, bad_alloc exception is catched by (exception &e) instead of (bad_alloc &e).
-    // Polymorphism. The subclass must be before its parent class. Otherwise, it is useless like
-    // catch (bad_alloc &e) here.
-    // The correct code is below.
-    // } catch (bad_alloc &e) {
-    //     cout << "Catching bad_alloc: " << e.what() << endl;
-    // } catch (exception &e) {
-    //     cout << "Catching exception: " << e.what() << endl;
-    // }
+    /**
+     * Importance of catching order of exceptions. `bad_alloc()` class is the subclasss
+     * of `exception()` class. Thus, `bad_alloc` exception is catched by 
+     * `(exception &e)` instead of `(bad_alloc &e)` Polymorphism. The subclass must be
+     * before its parent class. Otherwise, `catch (bad_alloc &e)` becomes useless
+     * because `catch (exception &e)` is before it.
+     * 
+     * The correct should is below:
+     * try {
+     *     goes_wrong();
+     * } catch (bad_alloc &e) {
+     *     cout << "Catching exception: " << e.what() << endl;
+     * } catch (exception &e) {
+     *     cout << "Catching exception: " << e.what() << endl;
+     * }
+     */
     try {
-        goes_wrong();
+        goes_wrong();  // Throw `bad_alloc()`.
     } catch (exception &e) {
         cout << "Catching exception: " << e.what() << endl;
-    } catch (bad_alloc &e) {
+    } catch (bad_alloc &e) {  // This catch is useless.
         cout << "Catching bad_alloc: " << e.what() << endl;
     }
 
@@ -92,15 +111,32 @@ int main() {
         cout << e.what() << endl;
     }
 
+    // Throw an unknown exception while running.
+    try {
+        test.throw_unknown_exception();
+    } catch (exception &e) {
+        cout << e.what() << endl;
+    } catch (...) {
+        cout << "Caught an unknown exception." << endl;
+
+        // Log additional information
+        cout << "Exception caught. Logging stack trace and program state:" << endl;
+
+        // Log stack trace or other relevant information here...
+
+        // There is NOTHING I can do to insepct what the exception is thrown but rethrow it.
+        // throw;  // throw the same error again to be handled somewhere else
+    }
+
     // exception is thrown all the way out to main() through call stacks.
     try {
         uses_might_go_wrong();
     } catch (int e) {
         cout << "Error code:" << e << endl;
     } catch (char const *e) {
-        cout << "Error message: " << e << endl;
+        cout << "string literal error message: " << e << endl;
     } catch (string &e) { // Use e or &e?
-        cout << "string error message: " << e << endl;
+        cout << "std::string error message: " << e << endl;
     }
 
     // Use try/catch to catch exception while allocating memories.
