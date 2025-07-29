61cef3c108125c338fb726e9ccb07779adf6a15a
lai <lai>
2025-07-29 16:39:20 +0200

    overload () operator

--- multithreading/05_threads_with_callable_objects.cpp
+++ multithreading/05_threads_with_callable_objects.cpp
@@ -2,36 +2,32 @@
 #include <mutex>
 #include <thread>
 
-using namespace std;
-
 /**
  * A callable object is an object of a class that overloads the call operator "()".
  */
 class App {
   private:
     int count = 0;
-    mutex mtx;
+    std::mutex mtx;
 
   public:
     void operator()() {
         for (int i = 0; i < 1e6; i++) {
-            lock_guard<mutex> guard(mtx);
+            std::lock_guard<std::mutex> guard(mtx);
             ++count;
         }
     }
 
-    int get_count() {
-        return count;
-    }
+    int get_count() { return count; }
 };
 
-// Example command: g++ -Wall -std=c++17 -pthread 05_threads_with_callable_objects.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 05_threads_with_callable_objects.cpp && ./a.out
 int main() {
     App app;
     app();
 
-    thread t1(ref(app)); // Pass the reference wrapper object with which thread can work.
-    thread t2(ref(app));
+    std::thread t1(std::ref(app)); // Pass the reference wrapper object with which thread can work.
+    std::thread t2(std::ref(app));
 
     t1.join();
     t2.join();
@@ -39,6 +35,6 @@ int main() {
     /**
      * The output 3000000 is comprised of app(), thread t1(ref(app)), thread t2(ref(app)).
      */
-    cout << app.get_count() << endl; // 3000000
+    std::cout << app.get_count() << std::endl; // 3000000
     return 0;
 }
