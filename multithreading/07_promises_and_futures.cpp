0caf61e4773b5ff955102862fb82b18db8238860
lai <lai>
2025-07-29 16:58:45 +0200

    std promise and future

--- multithreading/07_promises_and_futures.cpp
+++ multithreading/07_promises_and_futures.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
-
 double calculate_pi(int terms) {
     /**
      * Leibniz formula for pi
@@ -13,7 +11,7 @@ double calculate_pi(int terms) {
     double sum = 0.0;
 
     for (int i = 0; i < terms; i++) {
-        int sign = pow(-1, i);
+        int sign = std::pow(-1, i);
         double term = 1.0 / (i * 2 + 1);
         sum += sign * term;
     }
@@ -21,13 +19,13 @@ double calculate_pi(int terms) {
     return sum * 4;
 }
 
-// Example command: g++ -Wall -std=c++17 -pthread 07_promises_and_futures.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 07_promises_and_futures.cpp && ./a.out
 int main() {
     /**
      * The promise is something like the producer, which is used to carry the result that it
      * creates.
      */
-    promise<double> promise;
+    std::promise<double> promise;
 
     auto do_calculation = [&](int terms) {
         auto result = calculate_pi(terms);
@@ -36,7 +34,7 @@ int main() {
         promise.set_value(result);
     };
 
-    thread t1(do_calculation, 1e6);
+    std::thread t1(do_calculation, 1e6);
 
     /**
      * On the consumer side, the side that gets the result does something with it. We've got a
@@ -44,10 +42,10 @@ int main() {
      * and get() is a blocking function. So "future.get()" won't return until you do
      * "promise.set_value(result);" in the thread. And that's why it probably doesn't matter even if
      * we put "t1.join();" down here, because this is going to wait until "future.get()" happens
-     * anyway.  
+     * anyway.
      */
-    future<double> future = promise.get_future();
-    cout << setprecision(15) << future.get() << endl;
+    std::future<double> future = promise.get_future();
+    std::cout << std::setprecision(15) << future.get() << std::endl;
 
     /**
      * "t1.join();" is still here. Otherwise there will be the error below.
