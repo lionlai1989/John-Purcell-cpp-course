3c4a3c4333bc222f2b167db6ba2b748a73de5937
lai <lai>
2025-07-29 16:32:47 +0200

    pass mutex as func parameter

--- multithreading/03_function_arguments.cpp
+++ multithreading/03_function_arguments.cpp
@@ -2,9 +2,7 @@
 #include <mutex>
 #include <thread>
 
-using namespace std;
-
-void work(int &count, mutex &mtx) {
+void work(int &count, std::mutex &mtx) {
     for (int i = 0; i < 1e6; i++) {
         mtx.lock();
         ++count;
@@ -12,19 +10,19 @@ void work(int &count, mutex &mtx) {
     }
 }
 
-// Example command: g++ -Wall -std=c++17 -pthread 03_function_arguments.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 03_function_arguments.cpp && ./a.out
 int main() {
     int count = 0;
-    mutex mtx;
+    std::mutex mtx;
 
     // std::thread passes parameters with call by value, but we want to pass a reference here. Thus
     // ref() is essential here.
-    thread t1(work, ref(count), ref(mtx));
-    thread t2(work, ref(count), ref(mtx));
+    std::thread t1(work, std::ref(count), std::ref(mtx));
+    std::thread t2(work, std::ref(count), std::ref(mtx));
 
     t1.join();
     t2.join();
-    cout << count << endl; // 2000000
+    std::cout << count << std::endl; // 2000000
 
     return 0;
 }
