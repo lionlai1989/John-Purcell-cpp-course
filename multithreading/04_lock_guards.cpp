dc5287473b6fd5a273a3dac50585e9079ebc90eb
lai <lai>
2025-07-29 16:35:39 +0200

    basic lock_guard

--- multithreading/04_lock_guards.cpp
+++ multithreading/04_lock_guards.cpp
@@ -2,9 +2,7 @@
 #include <mutex>
 #include <thread>
 
-using namespace std;
-
-void work(int &count, mutex &mtx) {
+void work(int &count, std::mutex &mtx) {
     for (int i = 0; i < 1e6; i++) {
         /**
          * RAII: Resource Acquisition is Initialization
@@ -13,7 +11,7 @@ void work(int &count, mutex &mtx) {
          * that variable should go out of scope for any reason, even if an exception is thrown, then
          * it will release the resource or it can be made to do that. So here we're going to use
          * lock_gurad instead of mutex.
-         * 
+         *
          * lock_guard is the same as calling lock on a mutex. BUT when it goes out of scope, this is
          * the different thing, it will release the lock automatically. E.g., it is going to go out
          * of scope when it hits the curly bracket of the for loop here. So every time we go around
@@ -21,24 +19,24 @@ void work(int &count, mutex &mtx) {
          * section exists between "lock_guard<mutex> guard(mtx);" and wherever the next sort of
          * closing bracket is.
          */
-        lock_guard<mutex> guard(mtx); // Start of the critical section.
+        std::lock_guard<std::mutex> guard(mtx); // Start of the critical section.
         // unique_lock<mutex> guard(mtx); unique_lock has the explicit unlock function.
 
         ++count;
     } // End of the critical section.
 }
 
-// Example command: g++ -Wall -std=c++17 -pthread 04_lock_guards.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 04_lock_guards.cpp && ./a.out
 int main() {
     int count = 0;
-    mutex mtx;
+    std::mutex mtx;
 
-    thread t1(work, ref(count), ref(mtx));
-    thread t2(work, ref(count), ref(mtx));
+    std::thread t1(work, std::ref(count), std::ref(mtx));
+    std::thread t2(work, std::ref(count), std::ref(mtx));
 
     t1.join();
     t2.join();
-    cout << count << endl; // 2000000
+    std::cout << count << std::endl; // 2000000
 
     return 0;
 }
