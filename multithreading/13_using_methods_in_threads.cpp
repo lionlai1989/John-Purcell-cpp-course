bdca6f60869d70cfc0569036a140bfabdc8bcee8
lai <lai>
2025-07-30 14:51:52 +0200

    use methods in threads

--- multithreading/13_using_methods_in_threads.cpp
+++ multithreading/13_using_methods_in_threads.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
-
-template <typename T>
-class BlockingQueue {
+template <typename T> class BlockingQueue {
   public:
-    void push(T t) {
-        cout << "Push" << endl;
-    }
+    void push(T t) { std::cout << "Push" << std::endl; }
 
-    void pop() {
-        cout << "Pop" << endl;
-    }
+    void pop() { std::cout << "Pop" << std::endl; }
 };
 
-// Example command: g++ -Wall -std=c++17 -pthread 13_using_methods_in_threads.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 13_using_methods_in_threads.cpp && ./a.out
 int main() {
     BlockingQueue<int> qu;
-    thread t1(&BlockingQueue<int>::push, &qu, 7);
-    thread t2(&BlockingQueue<int>::pop, &qu);
+    std::thread t1(&BlockingQueue<int>::push, &qu, 7);
+    std::thread t2(&BlockingQueue<int>::pop, &qu);
 
     t1.join();
     t2.join();
     /**
      * Output:
      * PushPop
-     * 
+     *
      */
 
     return 0;
