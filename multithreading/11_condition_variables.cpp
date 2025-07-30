bf6a3e991cdbd6f1772b4b0bed721f8afceba631
lai <lai>
2025-07-30 14:41:49 +0200

    basic condition variable

--- multithreading/11_condition_variables.cpp
+++ multithreading/11_condition_variables.cpp
@@ -3,21 +3,20 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
-
-// Example command: g++ -Wall -std=c++17 -pthread 11_condition_variables.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 11_condition_variables.cpp && ./a.out
 int main() {
-    condition_variable condition;
-    mutex mtx;
+    std::condition_variable condition;
+    std::mutex mtx;
     bool ready1 = false;
 
-    thread t1([&]() {
-        this_thread::sleep_for(chrono::milliseconds(2000));
+    std::thread t1([&]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
         /**
-         * The reason that unique_lock is used instead of lock_guard is that we need the unlock method.
+         * The reason that unique_lock is used instead of lock_guard is that we need the unlock
+         * method.
          */
-        unique_lock<mutex> lock1(mtx);
+        std::unique_lock<std::mutex> lock1(mtx);
 
         ready1 = true;
         lock1.unlock();
@@ -25,7 +24,7 @@ int main() {
         /**
          *  The idea is that we have a thread that are waiting for something to happen in the
          *  present thread and we can notify this thread to wake up and try to do whatever it is
-         *  waiting to be able to do. 
+         *  waiting to be able to do.
          */
         condition.notify_one();
     });
@@ -33,17 +32,20 @@ int main() {
     /**
      * The unique_lock lock2 here needs to get the lock associated with the same mutex as lock1's.
      * Namely, the constructors of these unique_lock objects, lock1 and lock2, needs to supply with
-     * the same mutex that we are going to lock on. 
+     * the same mutex that we are going to lock on.
      */
-    unique_lock<mutex> lock2(mtx);
+    std::unique_lock<std::mutex> lock2(mtx);
 
     while (!ready1) {
-        condition.wait(lock2);
-        cout << "Wait for ready1 ..." << endl;
+        std::cout << "Wait for ready1 ..." << std::endl;
         // Print "Wait for ready1 ..." only once, because "condition.wait(lock2);" waits for
         // notify_one().
+
+        condition.wait(lock2);
+
+        std::cout << "lock2 is unlocked ..." << std::endl;
     }
-    cout << "ready1: " << ready1 << endl; // ready1: 1
+    std::cout << "ready1: " << ready1 << std::endl; // ready1: 1
 
     t1.join(); // The function returns when the thread execution has completed.
 
