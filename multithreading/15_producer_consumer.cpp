80446ea02dd18a3c1728227f226aada1865f51ab
lai <lai>
2025-07-30 15:06:20 +0200

    blocking queue, producer and consumer

--- multithreading/15_producer_consumer.cpp
+++ multithreading/15_producer_consumer.cpp
@@ -4,53 +4,48 @@
 #include <queue>
 #include <thread>
 
-using namespace std;
-
 template <typename T>
 class BlockingQueue {
   private:
-    mutex _mtx;
-    condition_variable _cond;
+    std::mutex _mtx;
+    std::condition_variable _cond;
     int _max_size;
-    queue<T> _queue;
+    std::queue<T> _queue;
 
   public:
-    BlockingQueue(int max_size) : _max_size(max_size) {
-    }
+    BlockingQueue(int max_size) : _max_size(max_size) {}
 
     void push(T t) {
-        unique_lock<mutex> lock(_mtx);
+        std::unique_lock<std::mutex> lock1(_mtx); // Use unique_lock because we need unlock
 
-        _cond.wait(lock, [this]() { return _queue.size() < _max_size; });
+        _cond.wait(lock1, [this]() { return _queue.size() < _max_size; });
 
         _queue.push(t);
 
-        lock.unlock();
+        lock1.unlock();
 
         _cond.notify_one(); // If more than one, it is unspecified which of the threads is selected.
     }
 
     T pop() {
-        unique_lock<mutex> lock(_mtx);
+        std::unique_lock<std::mutex> lock2(_mtx);
 
-        _cond.wait(lock, [this]() { return !_queue.empty(); });
+        _cond.wait(lock2, [this]() { return !_queue.empty(); });
 
         T item = _queue.front();
         _queue.pop();
 
-        lock.unlock();
+        lock2.unlock();
 
         _cond.notify_one(); // If more than one, it is unspecified which of the threads is selected.
 
         return item;
     }
 
-    int size() {
-        return _queue.size();
-    }
+    int size() { return _queue.size(); }
 };
 
-// Example command: g++ -Wall -std=c++17 -pthread 15_producer_consumer.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 15_producer_consumer.cpp && ./a.out
 int main() {
     /**
      * In 14_containers_and_thread_safety.cpp, we show that the STL queue is not thread safe, and we
@@ -58,20 +53,20 @@ int main() {
      */
     BlockingQueue<int> qu(5);
 
-    thread t1([&]() {
+    std::thread t1([&]() {
         // Producer
         for (int i = 0; i < 10; i++) {
-            cout << "Pushing " << i << endl;
+            std::cout << "Pushing " << i << std::endl;
             qu.push(i);
-            cout << "queue size is " << qu.size() << endl;
+            std::cout << "queue size is " << qu.size() << std::endl;
         }
     });
 
-    thread t2([&]() {
+    std::thread t2([&]() {
         // Consumer
         for (int i = 0; i < 10; i++) {
             auto item = qu.pop();
-            cout << "Consumed " << item << endl;
+            std::cout << "Consumed " << item << std::endl;
         }
     });
 
@@ -80,39 +75,39 @@ int main() {
 
     /**
      * As we can see, the queue's size never exceeds 5. And the producer and consumer works pretty
-     * well. 
-     * 
+     * well.
+     *
      * Output:
-	 * Pushing 0
-	 * queue size is 1
-	 * Pushing 1
-	 * queue size is 2
-	 * Pushing 2
-	 * queue size is 3
-	 * Pushing 3
-	 * queue size is 4
-	 * Pushing 4
-	 * queue size is 5
-	 * Pushing 5
-	 * Consumed 0
-	 * Consumed 1
-	 * Consumed 2
-	 * Consumed 3
-	 * Consumed 4
-	 * Consumed 5
-	 * queue size is 0
-	 * Pushing 6
-	 * queue size is 1
-	 * Pushing 7
-	 * queue size is 1
-	 * Pushing 8
-	 * queue size is 2
-	 * Pushing 9
-	 * queue size is 3
-	 * Consumed 6
-	 * Consumed 7
-	 * Consumed 8
-	 * Consumed 9
+     * Pushing 0
+     * queue size is 1
+     * Pushing 1
+     * queue size is 2
+     * Pushing 2
+     * queue size is 3
+     * Pushing 3
+     * queue size is 4
+     * Pushing 4
+     * queue size is 5
+     * Pushing 5
+     * Consumed 0
+     * Consumed 1
+     * Consumed 2
+     * Consumed 3
+     * Consumed 4
+     * Consumed 5
+     * queue size is 0
+     * Pushing 6
+     * queue size is 1
+     * Pushing 7
+     * queue size is 1
+     * Pushing 8
+     * queue size is 2
+     * Pushing 9
+     * queue size is 3
+     * Consumed 6
+     * Consumed 7
+     * Consumed 8
+     * Consumed 9
      */
 
     return 0;
