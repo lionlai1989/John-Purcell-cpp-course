b59652d8bece61e42bd4d292cfd3f14ab828dcf0
lai <lai>
2025-08-01 00:22:52 +0200

    protect queue.size

--- multithreading/16_a_blocking_queue.cpp
+++ multithreading/16_a_blocking_queue.cpp
@@ -4,22 +4,19 @@
 #include <queue>
 #include <thread>
 
-using namespace std;
-
 template <typename T>
 class BlockingQueue {
   private:
-    mutex _mtx;
-    condition_variable _cond;
+    std::mutex _mtx;
+    std::condition_variable _cond;
     int _max_size;
-    queue<T> _queue;
+    std::queue<T> _queue;
 
   public:
-    BlockingQueue(int max_size) : _max_size(max_size) {
-    }
+    BlockingQueue(int max_size) : _max_size(max_size) {}
 
     void push(T t) {
-        unique_lock<mutex> lock(_mtx);
+        std::unique_lock<std::mutex> lock(_mtx);
 
         _cond.wait(lock, [this]() { return _queue.size() < _max_size; });
 
@@ -31,7 +28,7 @@ class BlockingQueue {
     }
 
     T front() {
-        unique_lock<mutex> lock(_mtx);
+        std::unique_lock<std::mutex> lock(_mtx);
 
         _cond.wait(lock, [this]() { return !_queue.empty(); });
 
@@ -39,7 +36,7 @@ class BlockingQueue {
     }
 
     void pop() {
-        unique_lock<mutex> lock(_mtx);
+        std::unique_lock<std::mutex> lock(_mtx);
 
         _cond.wait(lock, [this]() { return !_queue.empty(); });
 
@@ -51,38 +48,38 @@ class BlockingQueue {
     }
 
     int size() {
-        lock_guard<mutex> lock(_mtx);
+        std::lock_guard<std::mutex> lock(_mtx);
         return _queue.size();
     }
 };
 
-// Example command: g++ -Wall -std=c++17 -pthread 16_a_blocking_queue.cpp && ./a.out
+// g++ -Wall -std=c++17 -pthread 16_a_blocking_queue.cpp && ./a.out
 int main() {
     /**
      * In this tutorial, we are going to make a couple of adjustments to the code we've got in
      * 15_producer_consumer.cpp to finish off our blocking queue.
-     * 
+     *
      * Now, one thing we have to pay attention to is the size() method in BlockingQueue, because it
      * returns the size of queue with no synchronization. Thus, we add the front() method and modify
      * the size() and pop() methods.
      */
     BlockingQueue<int> qu(5);
 
-    thread t1([&]() {
+    std::thread t1([&]() {
         // Producer
         for (int i = 0; i < 10; i++) {
-            cout << "Pushing " << i << endl;
+            std::cout << "Pushing " << i << std::endl;
             qu.push(i);
-            cout << "queue size is " << qu.size() << endl;
+            std::cout << "queue size is " << qu.size() << std::endl;
         }
     });
 
-    thread t2([&]() {
+    std::thread t2([&]() {
         // Consumer
         for (int i = 0; i < 10; i++) {
             auto item = qu.front();
             qu.pop();
-            cout << "Consumed " << item << endl;
+            std::cout << "Consumed " << item << std::endl;
         }
     });
 
@@ -91,38 +88,38 @@ int main() {
 
     /**
      * As we can see, our blocking queue also works and are better than 15_producer_consumer.cpp.
-     * 
+     *
      * Output:
-	 * Pushing 0
-	 * queue size is 1
-	 * Pushing 1
-	 * queue size is 2
-	 * Pushing 2
-	 * queue size is 3
-	 * Pushing 3
-	 * queue size is 4
-	 * Pushing 4
-	 * queue size is 5
-	 * Pushing 5
-	 * Consumed 0
-	 * Consumed 1
-	 * Consumed 2
-	 * Consumed 3
-	 * Consumed 4
-	 * Consumed 5
-	 * queue size is 0
-	 * Pushing 6
-	 * queue size is 1
-	 * Pushing 7
-	 * queue size is 2
-	 * Pushing 8
-	 * queue size is 3
-	 * Pushing 9
-	 * queue size is 4
-	 * Consumed 6
-	 * Consumed 7
-	 * Consumed 8
-	 * Consumed 9
+     * Pushing 0
+     * queue size is 1
+     * Pushing 1
+     * queue size is 2
+     * Pushing 2
+     * queue size is 3
+     * Pushing 3
+     * queue size is 4
+     * Pushing 4
+     * queue size is 5
+     * Pushing 5
+     * Consumed 0
+     * Consumed 1
+     * Consumed 2
+     * Consumed 3
+     * Consumed 4
+     * Consumed 5
+     * queue size is 0
+     * Pushing 6
+     * queue size is 1
+     * Pushing 7
+     * queue size is 2
+     * Pushing 8
+     * queue size is 3
+     * Pushing 9
+     * queue size is 4
+     * Consumed 6
+     * Consumed 7
+     * Consumed 8
+     * Consumed 9
      */
 
     return 0;
